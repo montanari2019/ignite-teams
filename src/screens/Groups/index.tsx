3e15e2db4797a989d403d101c54df1a0bf265790
montanari2019 <ikaro.montanari31@gmail.com>
2024-04-24 10:24:04 -0400

    inicinado com style domcponents no react netive

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Container, Title } from "./styled";
+
+export function Group() {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
+}
